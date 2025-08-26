21e74fbe479757630a4969ba4365c252b819b661
ManaswiSaripella27 <manaswi.saripella18@gmail.com>
2025-08-26 12:30:56 +0530

    Update Perfectnumber.java

--- Perfectnumber.java
+++ Perfectnumber.java
@@ -1,25 +1,27 @@
-package oop_skill;
+//java program for checking perfect number 
+package CSE;
 import java.util.Scanner;
-public class Perfectnumber {
-	public static void main(String[] args)
-	{
-	int n,sum = 0;
-	Scanner sc = new Scanner(System.in);
-	System.out.print("Enter number=");
-	n = sc.nextInt();
-	for(int i = 1; i < n; i++)
-	{
-	  if (n % i == 0)
-	  {
-	    sum = sum + i;
-	  }
+public class PerfectNum {
+	public static void main(String[] args) {
+		int n,sum=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		n=sc.nextInt();
+		for(int i=1;i<n;i++) {
+			if(n%i==0)
+			{
+				sum=sum+i;
+			}
+		}
+		if(n==sum)
+		{
+			System.out.println("Perfect Number");
+		}
+		else
+		{
+			System.out.println("Not a perfect number");
+		}
 	}
-	if(n == sum)
-	{
-	  System.out.println("Perfect Number");
-	}
-	else {
-	  System.out.println("Not a Perfect Number");
-	}
-	}
+
 }
+
